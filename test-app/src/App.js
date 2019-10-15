3dccf97b76edcb128fc31ffcb171c0a1440778a6
Younes Ijichi <younes.ijichi@polymtl.ca>
2019-10-15 18:47:06 -0400

    indexeddb

--- test-app/src/App.js
+++ test-app/src/App.js
@@ -2,6 +2,24 @@ import React, { Component } from "react";
 import './App.css';
 import { Button } from 'reactstrap';
 import JSZip from 'jszip';
+import { initDB, useIndexedDB } from 'react-indexed-db';
+
+export const DBConfig = {
+  name: 'MyDB',
+  version: 1,
+  objectStoresMeta: [
+    {
+      store: 'people',
+      storeConfig: { keyPath: 'id', autoIncrement: true },
+      storeSchema: [
+        { name: 'name', keypath: 'name', options: { unique: false } },
+        { name: 'email', keypath: 'email', options: { unique: false } }
+      ]
+    }
+  ]
+};
+
+initDB(DBConfig);
 
 class App extends Component {
   constructor(props) {
@@ -86,7 +104,10 @@ class App extends Component {
 
       var zip = new JSZip();
       zip.loadAsync(base64String, {base64: true}).then(zip => {
-        console.log(zip); 
+        console.log(zip);
+        useIndexedDB('people').add({ name: 'test', email: zip.files })
+        .then(() => {console.log("Added")})
+        .catch((e) => {console.log(e)});
       });
       })
       .catch((e) => {
@@ -134,29 +155,34 @@ class App extends Component {
   }
 
   showImage(e) {
-    console.log("Showing image");
-    // image as file text to file.
-    // var imageFileAsText = localStorage.getItem(this.lastFileName);
-    // var imageFile = new File([imageFileAsText], "test.jpg", {
-    //   type: "image/jpeg",
-    // });
-    // this.setState({
-    //   file: URL.createObjectURL(imageFile)
-    // })
+    // console.log("Showing image");
+    // // image as file text to file.
+    // // var imageFileAsText = localStorage.getItem(this.lastFileName);
+    // // var imageFile = new File([imageFileAsText], "test.jpg", {
+    // //   type: "image/jpeg",
+    // // });
+    // // this.setState({
+    // //   file: URL.createObjectURL(imageFile)
+    // // })
 
-    // let arrayBuffer = atob(localStorage.getItem(App.lastFileName));
+    // // let arrayBuffer = atob(localStorage.getItem(App.lastFileName));
 
-    // let imagefile = new File([arrayBuffer], "test.jpg", { type: "image/jpeg", });
+    // // let imagefile = new File([arrayBuffer], "test.jpg", { type: "image/jpeg", });
 
-    // let testFile = new File([arrayBuffer], "RandomName.jpg");
+    // // let testFile = new File([arrayBuffer], "RandomName.jpg");
 
-    // console.log(imagefile);
-    // console.log(testFile);
+    // // console.log(imagefile);
+    // // console.log(testFile);
 
-    const b64string = localStorage.getItem(localStorage.getItem(App.lastFileNameKey));
-    console.log(b64string)
-    const imageSrc = "data:image/jpeg;base64," + b64string
-    this.setState({ imagesrc: imageSrc });
+    // const b64string = localStorage.getItem(localStorage.getItem(App.lastFileNameKey));
+    // console.log(b64string)
+    // const imageSrc = "data:image/jpeg;base64," + b64string
+    // this.setState({ imagesrc: imageSrc });
+
+    // useIndexedDB('people').add({ name: 'name', email: 'email' })
+    // .then(() => {console.log("Added")})
+    // .catch((e) => {console.log(e)});
+
   }
 
   render() { 
